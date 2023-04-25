02161ad4cae1a6a7fe8ac7cf440a2c801d4c3772
João Vitor <joaovitornascimento@gmail.com>
2023-04-25 19:52:55 -0300

    adding elements in javascript

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const pokemonName = document.querySelector('.pokemon-name') // Nome do pokemon
+const pokemonNumber = document.querySelector('.pokemon-number') // ID do pokemon
+const pokemonImage = document.querySelector('.pokemon-image') // Gif do pokemon
+
+const form = document.querySelector('.form') 
+const input = document.querySelector('.input-search') // // Input de pesquisa por pokemon
+
+const buttonPrev = document.querySelector('.btn-prev') // Botão de voltar
+const buttonNext = document.querySelector('.btn-next') // Botão de avançar
