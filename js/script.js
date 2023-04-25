3f3652123faffbd3b695ef4877d7c8a0b39620a7
João Vitor <joaovitornascimento@gmail.com>
2023-04-25 19:56:18 -0300

    adding the request with the API

--- js/script.js
+++ js/script.js
@@ -7,3 +7,16 @@ const input = document.querySelector('.input-search') // // Input de pesquisa po
 
 const buttonPrev = document.querySelector('.btn-prev') // Botão de voltar
 const buttonNext = document.querySelector('.btn-next') // Botão de avançar
+
+let searchPokemon = 1; // Valor inicial do ID do pokemon
+
+// Criando uma requisição assíncrona
+const fetchPokemon = async (pokemon) => {
+    const APIResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    
+    // Se a requisição ocorrer, execute:
+    if (APIResponse.status == 200) {
+        const data = await APIResponse.json()
+        return data;
+    }
+}
